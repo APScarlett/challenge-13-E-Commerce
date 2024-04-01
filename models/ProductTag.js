973779c13c6fd6ccaf565e98589e3e00494ae33f
Alexander Scarlett <ascarlett92@yahoo.com>
2024-04-01 14:04:17 -0400

    added multiple models and their requirements into the DB model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,24 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+
+    product_name:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    price:{
+      type:DataTypes.DECIMAL,
+      allowNull:false,
+      validate:{  //this is used to validate the decimal
+        isDecimal:true, 
+      }
+    }
   },
   {
     sequelize,
